ed1e7b82e56ec46d1e8069e25a005b19fea7f40d
Matthew General <mgeneral@synacor.com>
2018-10-18 01:35:59 +0000

    Update root api listing

--- admin/root.go
+++ admin/root.go
@@ -15,10 +15,13 @@ func (a *Handler) RootHandler(w http.ResponseWriter, r *http.Request) {
 
 	root.Self("/")
 	root.AddLink("Auth", &haljson.Link{Href: "/auth"})
+	root.AddLink("Refresh token", &haljson.Link{Href: "/refresh"})
 	root.AddLink("Users", &haljson.Link{Href: "/users"})
-	root.AddLink("Article", &haljson.Link{Href: "/articles/{category}/{id:[a-zA-Z-_]+}", Templated: true})
+	root.AddLink("User", &haljson.Link{Href: "/users/{user}", Templated: true})
+	root.AddLink("Article", &haljson.Link{Href: "/articles/{id:[a-zA-Z-_]+}", Templated: true})
 	root.AddLink("Articles", &haljson.Link{Href: "/articles"})
-	root.AddLink("Article Category", &haljson.Link{Href: "/articles/{category}", Templated: true})
+	root.AddLink("Articles by Category", &haljson.Link{Href: "/categories/{category}", Templated: true})
+	root.AddLink("Categories", &haljson.Link{Href: "/categories"})
 	json, err := json.Marshal(root)
 	if err != nil {
 		log.Println(err)
