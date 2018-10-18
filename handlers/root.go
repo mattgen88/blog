99ae82bd03f1c616d3b8a08983b520cc5116b440
Matthew General <mgeneral@synacor.com>
2018-10-18 00:54:52 +0000

    Add endpoints to root

--- handlers/root.go
+++ handlers/root.go
@@ -14,9 +14,11 @@ func (h *Handler) RootHandler(w http.ResponseWriter, r *http.Request) {
 
 	root.Self("/")
 	root.AddLink("Users", &haljson.Link{Href: "/users"})
-	root.AddLink("Article", &haljson.Link{Href: "/articles/{category}/{id:[a-zA-Z-_]+}", Templated: true})
+	root.AddLink("User", &haljson.Link{Href: "/users/{user}", Templated: true})
+	root.AddLink("Article", &haljson.Link{Href: "/articles/{id:[a-zA-Z-_]+}", Templated: true})
 	root.AddLink("Articles", &haljson.Link{Href: "/articles"})
-	root.AddLink("Article Category", &haljson.Link{Href: "/articles/{category}", Templated: true})
+	root.AddLink("Article for Category", &haljson.Link{Href: "/categories/{category}", Templated: true})
+	root.AddLink("Categories", &haljson.Link{Href: "/categories"})
 	json, err := json.Marshal(root)
 	if err != nil {
 		log.Println(err)
